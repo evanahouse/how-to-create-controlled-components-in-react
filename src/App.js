314a5eb5e95f6d9a0ac0913e64cb42ba066d6436
Evan <evanahouse@gmail.com>
2021-03-31 11:47:44 -0700

    initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    catName:"",
+    catBreed:""
+  }
+  
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <form className="catForm">
+          <label htmlFor="name">Cat Name</label>
+          <input 
+            type="text" 
+            name="catName" 
+            id="name" 
+            value={this.state.catName} 
+            onChange={this.handleChange} 
+            />
+          <label htmlFor="breed">Cat Breed</label>
+          <input 
+            type="text" 
+            name="catBreed" 
+            value={this.state.catBreed} 
+            onChange={this.handleChange} 
+            />
+        </form> 
+      </div>
+    )
+    }
 }
 
 export default App;
